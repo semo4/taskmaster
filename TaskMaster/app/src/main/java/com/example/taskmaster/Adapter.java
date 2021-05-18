b63a6adef79fe86a921d1cbc5ab6126271f7b48d
Osama yousef <fotboy1788@hotmail.com>
2021-05-18 13:19:48 +0300

    add lab 28

---
+++ TaskMaster/app/src/main/java/com/example/taskmaster/Adapter.java
@@ -0,0 +1,69 @@
+package com.example.taskmaster;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Holder>{
+
+    Context c ;
+    ArrayList<Task> models;
+
+    public Adapter(Context c, ArrayList<Task> models) {
+        this.c = c;
+        this.models = models;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v =  LayoutInflater.from(parent.getContext()).inflate(R.layout.task_model,null);
+        return new Holder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        holder.title.setText(models.get(position).getTitle());
+        holder.body.setText(models.get(position).getBody());
+        holder.state.setText(models.get(position).getState());
+
+        //animation to determain the display the view
+
+        Animation animation = AnimationUtils.loadAnimation(c,android.R.anim.slide_in_left);
+        //start animation
+        holder.itemView.startAnimation(animation);
+        // use when you want to put each item data to same activity
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onItemClick(View v, int pos) {
+                //get data from item clicked
+                String title = models.get(pos).getTitle();
+                String body = models.get(pos).getBody();
+                String state = models.get(pos).getState();
+
+
+
+                Intent intent = new Intent(c , TaskDetail.class);
+                intent.putExtra("title", title);
+                intent.putExtra("body", body);
+                intent.putExtra("state", state);
+
+                c.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return models.size();
+    }
+}
